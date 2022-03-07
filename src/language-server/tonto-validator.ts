0096ff752bd5f7974b439bf4ed67eabb77232cea
Matheus Lenke <matheuslenke@gmail.com>
2022-03-07 07:53:22 -0300

    First commit

---
+++ src/language-server/tonto-validator.ts
@@ -0,0 +1,38 @@
+import { ValidationAcceptor, ValidationCheck, ValidationRegistry } from 'langium';
+import { TontoAstType, Person } from './generated/ast';
+import { TontoServices } from './tonto-module';
+
+/**
+ * Map AST node types to validation checks.
+ */
+type TontoChecks = { [type in TontoAstType]?: ValidationCheck | ValidationCheck[] }
+
+/**
+ * Registry for validation checks.
+ */
+export class TontoValidationRegistry extends ValidationRegistry {
+    constructor(services: TontoServices) {
+        super(services);
+        const validator = services.validation.TontoValidator;
+        const checks: TontoChecks = {
+            Person: validator.checkPersonStartsWithCapital
+        };
+        this.register(checks, validator);
+    }
+}
+
+/**
+ * Implementation of custom validations.
+ */
+export class TontoValidator {
+
+    checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
+        if (person.name) {
+            const firstChar = person.name.substring(0, 1);
+            if (firstChar.toUpperCase() !== firstChar) {
+                accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
+            }
+        }
+    }
+
+}
