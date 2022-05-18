52c59fd6727db8ba38e743381919c456ecd1bd60
Matheus Lenke Coutinho <matheuslenke@gmail.com>
2022-05-18 16:47:49 -0300

    feat: validate button sample

--- src/extension.ts
+++ src/extension.ts
@@ -5,9 +5,11 @@ import {
 } from 'vscode-languageclient/node';
 
 let client: LanguageClient;
+let myStatusBarItem: vscode.StatusBarItem;
 
 // This function is called when the extension is activated.
 export function activate(context: vscode.ExtensionContext): void {
+    createValidationCommands(context);
     client = startLanguageClient(context);
 }
 
@@ -56,4 +58,30 @@ function startLanguageClient(context: vscode.ExtensionContext): LanguageClient {
     // Start the client. This will also launch the server
     client.start();
     return client;
+}
+
+function createValidationCommands(context: vscode.ExtensionContext) {
+
+    const myCommandId = 'extension.validateModel';
+	context.subscriptions.push(vscode.commands.registerCommand(myCommandId, () => {
+		vscode.window.showInformationMessage(`Model validated!`);
+	}));
+
+	// create a new status bar item that we can now manage
+	myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+	myStatusBarItem.command = myCommandId;
+	context.subscriptions.push(myStatusBarItem);
+
+	// register some listener that make sure the status bar 
+	// item always up-to-date
+	context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(updateStatusBarItem));
+	context.subscriptions.push(vscode.window.onDidChangeTextEditorSelection(updateStatusBarItem));
+
+	// update status bar item once at start
+	updateStatusBarItem();
+}
+
+function updateStatusBarItem(): void {
+    myStatusBarItem.text = `$(cloud-upload) Validate Model on Cloud`;
+    myStatusBarItem.show();
 }
