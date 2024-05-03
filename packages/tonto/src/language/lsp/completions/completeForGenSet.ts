6f266fd8b96209d89597c2fd8fc1fc80bf740cda
Matheus Lenke <matheuslenke@gmail.com>
2024-05-03 12:37:31 -0300

    feat: bring snippets back

--- packages/tonto/src/language/lsp/completions/completeForGenSet.ts
+++ packages/tonto/src/language/lsp/completions/completeForGenSet.ts
@@ -1,43 +1,42 @@
 
-// import { CompletionItemKind } from "vscode-languageserver";
-// import { InsertTextFormat } from "vscode-languageserver";
+import { CompletionItemKind, InsertTextFormat } from "vscode-languageserver";
 
-import { CompletionAcceptor, NextFeature } from "langium/lsp";
+import { CompletionAcceptor, CompletionContext, NextFeature } from "langium/lsp";
 
-function completeForGenSetSnippets(next: NextFeature, acceptor: CompletionAcceptor) {
-    // acceptor({
-    //   label: "genset",
-    //   kind: CompletionItemKind.Snippet,
-    //   detail: "Basic internal association",
-    //   sortText: "100",
-    //   insertText: ["genset ${1:name} {", "\tgeneral ${2:Superclass}", "\tspecifics ${3:Subclass}", "}"].join("\n"),
-    //   insertTextFormat: InsertTextFormat.Snippet,
-    // });
+function completeForGenSetSnippets(context: CompletionContext, next: NextFeature, acceptor: CompletionAcceptor) {
+    acceptor(context, {
+      label: "genset",
+      kind: CompletionItemKind.Snippet,
+      detail: "Basic internal association",
+      sortText: "100",
+      insertText: ["genset ${1:name} {", "\tgeneral ${2:Superclass}", "\tspecifics ${3:Subclass}", "}"].join("\n"),
+      insertTextFormat: InsertTextFormat.Snippet,
+    });
 
-    // acceptor({
-    //   label: "complete-genset",
-    //   kind: CompletionItemKind.Snippet,
-    //   detail: "Basic internal association",
-    //   sortText: "100",
-    //   insertText: ["complete genset ${1:name} {", "\tgeneral ${2:Superclass}", "\tspecifics ${3:Subclass}", "}"].join(
-    //     "\n"
-    //   ),
-    //   insertTextFormat: InsertTextFormat.Snippet,
-    // });
+    acceptor(context, {
+      label: "complete-genset",
+      kind: CompletionItemKind.Snippet,
+      detail: "Basic internal association",
+      sortText: "100",
+      insertText: ["complete genset ${1:name} {", "\tgeneral ${2:Superclass}", "\tspecifics ${3:Subclass}", "}"].join(
+        "\n"
+      ),
+      insertTextFormat: InsertTextFormat.Snippet,
+    });
 
-    // acceptor({
-    //   label: "disjoint-complete-genset",
-    //   kind: CompletionItemKind.Snippet,
-    //   detail: "Basic internal association",
-    //   sortText: "100",
-    //   insertText: [
-    //     "disjoint complete genset ${1:name} {",
-    //     "\tgeneral ${2:Superclass}",
-    //     "\tspecifics ${3:Subclass}",
-    //     "}",
-    //   ].join("\n"),
-    //   insertTextFormat: InsertTextFormat.Snippet,
-    // });
+    acceptor(context, {
+      label: "disjoint-complete-genset",
+      kind: CompletionItemKind.Snippet,
+      detail: "Basic internal association",
+      sortText: "100",
+      insertText: [
+        "disjoint complete genset ${1:name} {",
+        "\tgeneral ${2:Superclass}",
+        "\tspecifics ${3:Subclass}",
+        "}",
+      ].join("\n"),
+      insertTextFormat: InsertTextFormat.Snippet,
+    });
 }
 
 export { completeForGenSetSnippets };
