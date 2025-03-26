b56c06ba4b2ca63e3f382eec836ec4ef7bf6e367
Matheus Lenke <matheuslenke@gmail.com>
2025-03-26 16:15:12 -0300

    fix: validators

--- packages/tonto/src/language/tonto-validator.ts
+++ packages/tonto/src/language/tonto-validator.ts
@@ -6,38 +6,38 @@ import type { TontoServices } from "./tonto-module.js";
  * Registry for validation checks.
  */
 export class TontoValidationRegistry extends ValidationRegistry {
-  constructor(services: TontoServices) {
-    super(services);
-    const validator = services.validation.TontoValidator;
-    const checks: ValidationChecks<TontoAstType> = {
-      // ClassDeclaration: [
-      //     validator.ClassDeclarationValidator.checkUltimateSortalSpecializeUltimateSortal,
-      //     validator.ClassDeclarationValidator.checkRigidSpecializesAntiRigid,
-      //     validator.ClassDeclarationValidator.checkDuplicatedReferenceNames,
-      //     validator.ClassDeclarationValidator.checkCompatibleNatures,
-      //     validator.ClassDeclarationValidator.checkClassWithoutStereotype,
-      //     validator.ClassDeclarationValidator.checkGeneralizationSortality,
-      // ],
-      // ContextModule: [
-      //     validator.ContextModuleValidator.checkDuplicatedClassName,
-      //     validator.ContextModuleValidator.checkCircularSpecialization,
-      //     validator.ContextModuleValidator.checkClassDeclarationShouldSpecializeUltimateSortal,
-      //     validator.ContextModuleValidator.checkCompatibleNaturesOfBaseSortals,
-      //     validator.ContextModuleValidator.checkSpecializationNatureRestrictions,
-      //     validator.ContextModuleValidator.checkRedundantNatures,
-      // ],
-      // GeneralizationSet: [
-      //     validator.GeneralizationValidator.checkCircularGeneralization,
-      //     validator.GeneralizationValidator.checkGeneralizationSetConsistency,
-      //     validator.GeneralizationValidator.checkGeneralizationSortality,
-      //     validator.GeneralizationValidator.checkRigidSpecializesAntiRigid,
-      //     validator.GeneralizationValidator.checkGeneralizationDataType,
-      // ],
-      // DataType: [
-      //     validator.ComplexDataTypeValidator.checkCompatibleNatures,
-      //     validator.ComplexDataTypeValidator.checkSpecialization,
-      // ],
-    };
-    this.register(checks, validator);
-  }
+    constructor(services: TontoServices) {
+        super(services);
+        const validator = services.validation.TontoValidator;
+        const checks: ValidationChecks<TontoAstType> = {
+            ClassDeclaration: [
+                validator.ClassDeclarationValidator.checkUltimateSortalSpecializeUltimateSortal,
+                validator.ClassDeclarationValidator.checkRigidSpecializesAntiRigid,
+                validator.ClassDeclarationValidator.checkDuplicatedReferenceNames,
+                validator.ClassDeclarationValidator.checkCompatibleNatures,
+                validator.ClassDeclarationValidator.checkClassWithoutStereotype,
+                validator.ClassDeclarationValidator.checkGeneralizationSortality,
+            ],
+            ContextModule: [
+                validator.ContextModuleValidator.checkDuplicatedClassName,
+                validator.ContextModuleValidator.checkCircularSpecialization,
+                validator.ContextModuleValidator.checkClassDeclarationShouldSpecializeUltimateSortal,
+                validator.ContextModuleValidator.checkCompatibleNaturesOfBaseSortals,
+                validator.ContextModuleValidator.checkSpecializationNatureRestrictions,
+                validator.ContextModuleValidator.checkRedundantNatures,
+            ],
+            GeneralizationSet: [
+                validator.GeneralizationValidator.checkCircularGeneralization,
+                validator.GeneralizationValidator.checkGeneralizationSetConsistency,
+                validator.GeneralizationValidator.checkGeneralizationSortality,
+                validator.GeneralizationValidator.checkRigidSpecializesAntiRigid,
+                validator.GeneralizationValidator.checkGeneralizationDataType,
+            ],
+            DataType: [
+                validator.ComplexDataTypeValidator.checkCompatibleNatures,
+                validator.ComplexDataTypeValidator.checkSpecialization,
+            ],
+        };
+        this.register(checks, validator);
+    }
 }
